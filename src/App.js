61fa160a76292295e4ac26c3d8ddddc656e18034
BaptistePayan <teck-bat@hotmail.fr>
2025-10-16 17:41:25 +0200

    mise en place d'un props pour les compétences

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import User from './components/User';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <div className="gird_container">
         <div className="sidebar">
-          <User/>
+          <User />
+          <Skills />
         </div>
         <div className="main">main</div>
      </div>
---
+++ src/components/Skills.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Skills.css';
+import Skill from './Skill';
+
+function Skills() {
+  return (
+    <>
+    <div className='skills'>
+        <h2 className='h2'>Compétences</h2>
+        <Skill title="HTML" rating="4" />
+        <Skill title="CSS" rating="4" />
+        <Skill title="SQL" rating="3" />
+        <Skill title="JavaScript" rating="3" />
+        <Skill title="React" rating="3" />
+        <Skill title="Node.js" rating="2" />
+        <Skill title="Vue.js" rating="2" />
+        <Skill title="PHP" rating="2" />
+
+    </div>
+    <div className='skills'>
+        <h2 className='h2'>Langues</h2>
+        <Skill title="Français" rating="5" />
+        <Skill title="Anglais" rating="3" />
+    </div>
+    interests
+    </>
+  )
+}
+export default Skills;
